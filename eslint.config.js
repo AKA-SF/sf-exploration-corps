54f04b3936788865b1b835bbdd0d218ea66afed0
Convergence Art Artscience_lab Science LAB <artscience_lab@Convergenceui-iMac.local>
2026-06-15 00:26:49 +0900

    Remove local generator outputs from site workflow

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'av-generator-live/**']),
+  globalIgnores(['dist', 'av-generator-live/**', 'public/rendered-shorts/**']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
